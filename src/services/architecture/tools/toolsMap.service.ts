6c7b04c42400eac17d555c4cff5e95020bed4618
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-02-06 16:09:47 -0500

    aa

--- src/services/architecture/tools/toolsMap.service.ts
+++
@@ -1,7 +0,0 @@
-import axios, { endpoints } from 'src/utils/axios';
-
-export const GetToolsTableMapByIdService = async (id: any) => {
-  const response = await axios.get<any>(`${endpoints.architecture.tools.map.byId}/${id}`);
-  return response;
-};
-
--- src/services/performance/related-data.service.ts
+++
@@ -1,9 +0,0 @@
-// utils
-import axios, { endpoints } from 'src/utils/axios';
-
-// ----------------------------------------------------------------------
-
-export const GetPerformanceRelatedDataService = async (params: any) => {
-  const response = await axios.get<any>(`${endpoints.performance.relatedData}`, { params });
-  return response;
-};
