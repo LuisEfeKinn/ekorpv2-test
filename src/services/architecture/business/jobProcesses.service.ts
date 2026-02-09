7d5503858f1338901dd00eb03c4f145fce36dec8
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-02-09 11:12:05 -0500

    erase

--- src/services/architecture/business/jobProcesses.service.ts
+++
@@ -1,7 +0,0 @@
-import axios, { endpoints } from 'src/utils/axios';
-
-export const SaveJobProcessRelationService = async (payload: any) => {
-  const response = await axios.post<any>(endpoints.architecture.business.jobProcesses, payload);
-  return response;
-};
-
