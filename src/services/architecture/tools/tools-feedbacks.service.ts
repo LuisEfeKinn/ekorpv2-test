ea05bc0a9b0a8653d77cafe8af3c78504f956678
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-02-10 18:03:27 -0500

    aaax

--- src/services/architecture/tools/tools-feedbacks.service.ts
+++
@@ -1,6 +0,0 @@
-import axios, { endpoints } from 'src/utils/axios';
-
-export const SaveToolFeedbackService = async (toolId: string, data: any) => {
-  const response = await axios.post(`${endpoints.architecture.tools.table.feedbacks}/${toolId}`, data);
-  return response;
-};
