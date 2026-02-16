2d26720c5e9b5d350db1ad520a03c92b90a77a4f
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-02-16 16:30:57 -0500

    :fire: xd

--- src/sections/performance/evaluation-responses/view/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './evaluation-my-results';
-
-export * from './evaluation-by-evaluator';
-
-export * from './evaluation-responses-view';
-
-export * from './evaluation-response-history';
-
-export * from './evaluation-responses-objectives';
