01f3ec77529a5b19e5a44f4fcdcf7b6148f3c0b8
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-02-24 17:15:40 -0500

    :fire: xd

--- src/sections/user-learning-path/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user-learning-paths-card';
-export * from './view/user-learning-paths-view';
-export * from './user-learning-paths-table-toolbar';
-export * from './user-learning-paths-table-filters-result';
