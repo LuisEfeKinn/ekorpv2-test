48b79f0ce5ae7947aa9e5a51cec8e427c11ca97b
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-03 16:30:10 -0500

    :fire: xd

--- src/sections/organization/view/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './organization-view';
-
-export * from './organization-edit';
-
-export * from './organization-create';
-
-// export * from './organizational-chart-view';
-
-export * from './organizational-chart-view-new';
