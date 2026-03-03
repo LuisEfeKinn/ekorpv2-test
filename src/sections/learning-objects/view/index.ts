4f0cf061a319bf22f910bc41361a5010d3ef7298
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-03 14:36:24 -0500

    :fire: xd

--- src/sections/learning-objects/view/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './learning-objects-view';
-
-export * from './learning-objects-edit';
-
-export * from './learning-objects-create';
-
-export * from './learning-objects-details-view';
