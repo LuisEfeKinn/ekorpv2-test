39d320b584a08bc40177ca10cb12a1a542a08f2c
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-04 17:56:42 -0500

    :fire: xd

--- src/sections/user-rewards/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user-rewards-cards';
-export * from './user-rewards-history-card';
-
-export { UserRewardsView } from './view/user-rewards-view';
-export { UserRewardsHistoryView } from './view/user-rewards-history';
