f81218e07e7dd720e52a1d06a11801b51411401c
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-09 16:59:45 -0500

    :fire: xd

--- src/sections/ai-course-generator/view/index.ts
+++
@@ -1,5 +0,0 @@
-// AI Course Generator Views
-export { AiCoursesView } from './ai-courses-view';
-export { AiCoursesEditView } from './ai-courses-edit';
-export { AiCoursesPreviewView } from './ai-courses-preview';
-export { AiCoursesCreateChatView } from './ai-courses-create-chat';
