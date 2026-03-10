e8f8b9ec89f6010bad4e8069452e25a55086ae8a
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-10 16:30:33 -0500

    :fire: xd

--- src/sections/integrations/view/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './integrations-view';
-export * from './integrations-edit';
-export * from './integrations-create';
-export * from './integrations-course-view';
-export * from './integrations-categorie-view';
