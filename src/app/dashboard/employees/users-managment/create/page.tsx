dcb30e13af27b16dc1152a83e66aa42a8e010fcf
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-10 18:21:44 -0500

    :fire: xd

--- src/app/dashboard/employees/users-managment/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserManagementCreateView } from 'src/sections/users-managment/view';
-
-// ----------------------------------------------------------------------
-
-export const metadata = { title: `Create Employee` };
-
-export default function Page() {
-  return <UserManagementCreateView />;
-}
--- src/sections/architecture/aplications/aplications-table/view/index.tsx
+++
@@ -1,7 +0,0 @@
-export { ApplicationTableView } from './application-table-view';
-
-export { ApplicationDiagramView } from './application-diagram-view';
-
-export { ApplicationTimelineView } from './application-timeline-viewt';
-
-export { ApplicationTableMapView } from './application-table-map-view';
