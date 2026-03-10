69915bec70b536458770e9cd436594db1eb27908
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-10 15:25:23 -0500

    :fire: xd

--- src/app/dashboard/organization/positions/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PositionCreateView } from 'src/sections/positions/view';
-
-// ----------------------------------------------------------------------
-
-export const metadata = { title: `Create Position` };
-
-export default function Page() {
-  return <PositionCreateView />;
-}
