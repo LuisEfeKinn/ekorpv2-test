f829d21faa107389e4aea40f4db1b19ae906284c
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-11 11:28:14 -0500

    :fire: xd

--- src/sections/ai-model-settings/index.ts
+++
@@ -1,8 +0,0 @@
-export { AiModelSettingsTableRow } from './ai-model-settings-table-row';
-export { AiModelSettingsNewEditForm } from './ai-model-settings-new-edit-form';
-export { AiModelSettingsTableToolbar } from './ai-model-settings-table-toolbar';
-export {
-  AiModelSettingsView,
-  AiModelSettingsEditView,
-  AiModelSettingsCreateView,
-} from './view';
--- src/sections/architecture/infrastructure/infrastructure-table/index.ts
+++
@@ -1,3 +0,0 @@
-export { InfrastructureTableDiagram } from './infrastructure-table-diagram';
-export { InfrastructureTableExpandedDiagram } from './infrastructure-table-expanded-diagram';
-export { InfrastructureTableNodeCreateModal } from './infrastructure-table-node-create-modal';
