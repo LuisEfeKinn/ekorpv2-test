3ee849d308b847b0978f4329b4e621556abf77fc
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-13 16:17:37 -0500

    :fire: xd

--- src/services/employees/employment-payment-period.service.ts
+++
@@ -1,9 +0,0 @@
-// utils
-import axios, { endpoints } from 'src/utils/axios';
-
-// ----------------------------------------------------------------------
-
-export const GetEmployeesPaymentPeriods = async (params: any) => {
-  const response = await axios.get<any>(`${endpoints.employees.paymentPeriods.all}`, { params });
-  return response;
-};
