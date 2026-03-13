1643f09027c458b294aa716c14c4aea221a41601
LuisEfeKinn <luis.diaz@kamilainnovation.com>
2026-03-13 09:17:36 -0500

    :fire: xd

--- src/app/dashboard/architecture/business/risks/table/map/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RiskTableMapView } from 'src/sections/architecture/risk/risk-table/view';
-
-type Params = { id: string };
-type Props = { params: Promise<Params> };
-
-export default async function Page({ params }: Props) {
-  const { id } = await params;
-  return <RiskTableMapView id={id} />;
-}
